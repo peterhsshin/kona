d120c8e28cdb5fd82a81c43a05e466ae7348ffec
Peter Shin <peter@canvasslabas.com>
2022-02-07 05:55:02 -0800

    add header (.h) files.

---
+++ bootable/bootloader/edk2/EmbeddedPkg/Include/libfdt_env.h
@@ -0,0 +1,206 @@
+/** @file
+*
+*  Copyright (c) 2011-2014, ARM Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef _LIBFDT_ENV_H
+#define _LIBFDT_ENV_H
+
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Uefi.h>
+
+typedef UINT16 fdt16_t;
+typedef UINT32 fdt32_t;
+typedef UINT64 fdt64_t;
+
+typedef UINT8 uint8_t;
+typedef UINT16 uint16_t;
+typedef UINT32 uint32_t;
+typedef UINT64 uint64_t;
+typedef UINTN uintptr_t;
+typedef UINTN size_t;
+typedef BOOLEAN bool;
+
+static inline uint16_t fdt16_to_cpu(fdt16_t x)
+{
+  return SwapBytes16 (x);
+}
+#define cpu_to_fdt16(x) fdt16_to_cpu(x)
+
+static inline uint32_t fdt32_to_cpu(fdt32_t x)
+{
+  return SwapBytes32 (x);
+}
+#define cpu_to_fdt32(x) fdt32_to_cpu(x)
+
+static inline uint64_t fdt64_to_cpu(fdt64_t x)
+{
+  return SwapBytes64 (x);
+}
+#define cpu_to_fdt64(x) fdt64_to_cpu(x)
+
+static inline void* memcpy(void* dest, const void* src, size_t len) {
+  return CopyMem (dest, src, len);
+}
+
+static inline void *memmove(void *dest, const void *src, size_t n) {
+  return CopyMem (dest, src, n);
+}
+
+static inline void *memset(void *s, int c, size_t n) {
+  return SetMem (s, n, c);
+}
+
+static inline int memcmp(const void* dest, const void* src, int len) {
+  return CompareMem (dest, src, len);
+}
+
+static inline void *memchr(const void *s, int c, size_t n) {
+  return ScanMem8 (s, n, c);
+}
+
+static inline size_t strlen (const char* str) {
+  return AsciiStrLen (str);
+}
+
+static inline char *strchr(const char *s, int c) {
+  char pattern[2];
+  pattern[0] = c;
+  pattern[1] = 0;
+  return AsciiStrStr (s, pattern);
+}
+
+static inline int strcmp(const char *s1, const char *s2)
+{
+  return (int)AsciiStrCmp( s1, s2);
+}
+
+static inline int strncmp(const char *s1, const char *s2, size_t n)
+{
+  return (int)AsciiStrnCmp( s1, s2, n);
+}
+
+/**
+  Simple character classification routines, corresponding to POSIX class names
+  and ASCII encoding.
+**/
+#define toupper(a)  ((((a) >= 'a') && ((a) <= 'z')) ? ((a) - 'a' + 'A') : (a))
+
+#define isalpha(chr) (('a' <= chr && chr <= 'z') || ('A' <= chr && chr <= 'Z'))
+STATIC
+BOOLEAN
+isalnum (
+  IN  CHAR8 Chr
+  )
+{
+  return (('0' <= Chr && Chr <= '9') ||
+          ('A' <= Chr && Chr <= 'Z') ||
+          ('a' <= Chr && Chr <= 'z')
+          );
+}
+
+static int
+Digit2Val( int c)
+{
+  if(isalpha(c)) {  /* If c is one of [A-Za-z]... */
+    c = toupper(c) - 7;   // Adjust so 'A' is ('9' + 1)
+  }
+  return c - '0';   // Value returned is between 0 and 35, inclusive.
+}
+
+/* Determines if a particular character represents a space character */
+static inline int isspace (int c)
+{
+  //
+  // <space> ::= [ ]
+  //
+  return ((c) == ' ');
+}
+
+#define UINT32_MAX      0xffffffffU                     /* uint32_t       */
+/** The strtoul function converts the initial portion of the string pointed to
+    by nptr to unsigned long int representation.
+
+    See the description for strtol for more information.
+
+  @return   The strtoul function returns the converted value, if any. If no
+            conversion could be performed, zero is returned. If the correct
+            value is outside the range of representable values, ULONG_MAX is
+            returned and the value of the macro ERANGE is stored in errno.
+**/
+
+static inline unsigned long
+strtoul(const char * __restrict nptr, char ** __restrict endptr, int base)
+{
+  const char     *pEnd;
+  unsigned long   Result = 0;
+  unsigned long   Previous;
+  int             temp;
+
+  pEnd = nptr;
+
+  if((base < 0) || (base == 1) || (base > 36)) {
+    if(endptr != NULL) {
+    *endptr = NULL;
+    }
+    return 0;
+  }
+  // Skip leading spaces.
+  while(isspace(*nptr))   ++nptr;
+
+  // Process Subject sequence: optional + sign followed by digits.
+  if(*nptr == '+') {
+    ++nptr;
+  }
+
+  if(*nptr == '0') {  /* Might be Octal or Hex */
+    if(toupper(nptr[1]) == 'X') {   /* Looks like Hex */
+      if((base == 0) || (base == 16)) {
+        nptr += 2;  /* Skip the "0X"      */
+        base = 16;  /* In case base was 0 */
+      }
+    }
+    else {    /* Looks like Octal */
+      if((base == 0) || (base == 8)) {
+        ++nptr;     /* Skip the leading "0" */
+        base = 8;   /* In case base was 0   */
+      }
+    }
+  }
+  if(base == 0) {   /* If still zero then must be decimal */
+    base = 10;
+  }
+  if(*nptr  == '0') {
+    for( ; *nptr == '0'; ++nptr);  /* Skip any remaining leading zeros */
+    pEnd = nptr;
+  }
+
+  while( isalnum(*nptr) && ((temp = Digit2Val(*nptr)) < base)) {
+    Previous = Result;
+    Result = (Result * base) + (unsigned long)temp;
+    if( Result < Previous)  {   // If we overflowed
+      Result = UINT32_MAX;
+      //errno = -1;
+      break;
+    }
+    pEnd = ++nptr;
+  }
+
+  // Save pointer to final sequence
+  if(endptr != NULL) {
+    *endptr = (char *)pEnd;
+  }
+  return Result;
+}
+#endif /* _LIBFDT_ENV_H */
---
+++ bootable/bootloader/edk2/QcomModulePkg/Include/Library/BootImage.h
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _BOOT_IMAGE_H_
+#define _BOOT_IMAGE_H_
+
+#define BOOT_MAGIC "ANDROID!"
+#define BOOT_MAGIC_SIZE 8
+#define BOOT_NAME_SIZE 16
+#define BOOT_ARGS_SIZE 512
+#define BOOT_IMG_MAX_PAGE_SIZE 4096
+#define KERNEL64_HDR_MAGIC 0x644D5241 /* ARM64 */
+#define BOOT_EXTRA_ARGS_SIZE 1024
+
+#define BOOT_HEADER_VERSION_ZERO 0
+/* Struct def for boot image header
+ * Bootloader expects the structure of boot_img_hdr with header version
+ *  BOOT_HEADER_VERSION_ZERO to be as follows:
+ */
+struct boot_img_hdr_v0 {
+  CHAR8 magic[BOOT_MAGIC_SIZE];
+
+  UINT32 kernel_size; /* size in bytes */
+  UINT32 kernel_addr; /* physical load addr */
+
+  UINT32 ramdisk_size; /* size in bytes */
+  UINT32 ramdisk_addr; /* physical load addr */
+
+  UINT32 second_size; /* size in bytes */
+  UINT32 second_addr; /* physical load addr */
+
+  UINT32 tags_addr;  /* physical addr for kernel tags */
+  UINT32 page_size;  /* flash page size we assume */
+  UINT32 header_version; /* version for the boot image header */
+  UINT32 os_version; /* version << 11 | patch_level */
+
+  UINT8 name[BOOT_NAME_SIZE]; /* asciiz product name */
+
+  UINT8 cmdline[BOOT_ARGS_SIZE];
+
+  UINT32 id[8]; /* timestamp / checksum / sha1 / etc */
+
+  /* Supplemental command line data; kept here to maintain
+   * binary compatibility with older versions of mkbootimg
+   */
+  UINT8 extra_cmdline[BOOT_EXTRA_ARGS_SIZE];
+} __attribute__((packed));
+
+/*
+ * It is expected that callers would explicitly specify which version of the
+ * boot image header they need to use.
+ */
+typedef struct boot_img_hdr_v0 boot_img_hdr;
+
+/**
+ * Offset of recovery DTBO length in a boot image header of version V1 or
+ * above.
+ */
+#define BOOT_IMAGE_HEADER_V1_RECOVERY_DTBO_SIZE_OFFSET sizeof (boot_img_hdr)
+
+/*
+ * ** +-----------------+
+ * ** | boot header     | 1 page
+ * ** +-----------------+
+ * ** | kernel          | n pages
+ * ** +-----------------+
+ * ** | ramdisk         | m pages
+ * ** +-----------------+
+ * ** | second stage    | o pages
+ * ** +-----------------+
+ * ** n = (kernel_size + page_size - 1) / page_size
+ * ** m = (ramdisk_size + page_size - 1) / page_size
+ * ** o = (second_size + page_size - 1) / page_size
+ * ** 0. all entities are page_size aligned in flash
+ * ** 1. kernel and ramdisk are required (size != 0)
+ * ** 2. second is optional (second_size == 0 -> no second)
+ * ** 3. load each element (kernel, ramdisk, second) at
+ * **    the specified physical address (kernel_addr, etc)
+ * ** 4. prepare tags at tag_addr.  kernel_args[] is
+ * **    appended to the kernel commandline in the tags.
+ * ** 5. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
+ * ** 6. if second_size != 0: jump to second_addr
+ * **    else: jump to kernel_addr
+ * */
+
+#define BOOT_HEADER_VERSION_ONE 1
+
+struct boot_img_hdr_v1 {
+  UINT32 recovery_dtbo_size;   /* size in bytes for recovery DTBO image */
+  UINT64 recovery_dtbo_offset; /* physical load addr */
+  UINT32 header_size;
+} __attribute__((packed));
+
+/* When the boot image header has a version of BOOT_HEADER_VERSION_ONE,
+ * the structure of the boot image is as follows:
+ *
+ * +-----------------+
+ * | boot header     | 1 page
+ * +-----------------+
+ * | kernel          | n pages
+ * +-----------------+
+ * | ramdisk         | m pages
+ * +-----------------+
+ * | second stage    | o pages
+ * +-----------------+
+ * | recovery dtbo   | p pages
+ * +-----------------+
+ * n = (kernel_size + page_size - 1) / page_size
+ * m = (ramdisk_size + page_size - 1) / page_size
+ * o = (second_size + page_size - 1) / page_size
+ * p = (recovery_dtbo_size + page_size - 1) / page_size
+ *
+ * 0. all entities are page_size aligned in flash
+ * 1. kernel and ramdisk are required (size != 0)
+ * 2. recovery_dtbo is required for recovery.img
+ *    in non-A/B devices(recovery_dtbo_size != 0)
+ * 3. second is optional (second_size == 0 -> no second)
+ * 4. load each element (kernel, ramdisk, second, recovery_dtbo) at
+ *    the specified physical address (kernel_addr, etc)
+ * 5. prepare tags at tag_addr.  kernel_args[] is
+ *    appended to the kernel commandline in the tags.
+ * 6. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
+ * 7. if second_size != 0: jump to second_addr
+ *    else: jump to kernel_addr
+ */
+
+#define BOOT_IMAGE_HEADER_V2_OFFSET sizeof (struct boot_img_hdr_v1)
+#define BOOT_HEADER_VERSION_TWO 2
+
+struct boot_img_hdr_v2 {
+  UINT32 dtb_size; /* size in bytes for DTB image */
+  UINT64 dtb_addr; /* physical load address for DTB image */
+} __attribute__((packed));
+
+/* When the boot image header has a version of BOOT_HEADER_VERSION_TWO,
+ * the structure of the boot image is as follows:
+ *
+ * +-----------------+
+ * | boot header     | 1 page
+ * +-----------------+
+ * | kernel          | n pages
+ * +-----------------+
+ * | ramdisk         | m pages
+ * +-----------------+
+ * | second stage    | o pages
+ * +-----------------+
+ * | recovery dtbo   | p pages
+ * +-----------------+
+ * | dtb.img         | q pages
+ * +-----------------+
+ *
+ * n = (kernel_size + page_size - 1) / page_size
+ * m = (ramdisk_size + page_size - 1) / page_size
+ * o = (second_size + page_size - 1) / page_size
+ * p = (recovery_dtbo_size + page_size - 1) / page_size
+ * q = (dtb_size + page_size - 1) / page_size
+ *
+ * 0. all entities are page_size aligned in flash
+ * 1. kernel and ramdisk are required (size != 0)
+ * 2. recovery_dtbo is required for recovery.img (recovery_dtbo_size != 0)
+ * 3. second is optional (second_size == 0 -> no second)
+ * 4. dtb.img has all the dtbs catted one after the other
+ * 5. load each element (kernel, ramdisk, second, recovery_dtbo) at
+ *    the specified physical address (kernel_addr, etc)
+ * 6. prepare tags at tag_addr.  kernel_args[] is
+ *    appended to the kernel commandline in the tags.
+ * 7. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
+ * 8. if second_size != 0: jump to second_addr
+ *    else: jump to kernel_addr
+ */
+
+struct kernel64_hdr {
+  UINT32 Code0;       /* Executable code */
+  UINT32 Code1;       /* Executable code */
+  UINT64 TextOffset; /* Image load offset, little endian */
+  UINT64 ImageSize;  /* Effective Image size, little endian */
+  UINT64 Flags;       /* kernel flags, little endian */
+  UINT64 Res2;        /* reserved */
+  UINT64 Res3;        /* reserved */
+  UINT64 Res4;        /* reserved */
+  UINT32 magic_64;    /* Magic number, little endian, "ARM\x64" i.e 0x644d5241*/
+  UINT32 Res5;        /* reserved (used for PE COFF offset) */
+};
+
+typedef struct kernel64_hdr Kernel64Hdr;
+
+#endif
